ec715baf644477c36552d67de0fa27f57fbe01b4
MONISH_TYAGI <monishtyagi5770@gmail.com>
2022-10-07 19:05:00 +0530

    7oct22Second

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,12 +1,23 @@
 import React from 'react'
-
+import  ReactDOM  from 'react-dom'
+import "./Video.css"
 const Video = (props) => {
-    console.log("video ",props);
-    console.log("hello")
+    const handleClick=(e)=>{
+        e.preventDefault();
+        e.target.muted=!e.target.muted;
+    } 
+    const handleScroll=(e)=>{
+        let next=ReactDOM.findDOMNode(e.target).parentNode.nextSibling
+        if(next)
+        {
+            next.scrollIntoView()
+            e.target.muted=true;
+        }
+    }
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+    // <div className="video-div" >
+    <video src={props.src} onEnded={handleScroll} className="videos-styling" muted="muted" onClick={handleClick} />
+    // </div>
 
   )
     
